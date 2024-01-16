e87d26d7855fc4ae96730f97c27687888273abe1
RisaKikuchi <risa.kikuchi@rakus-partners.co.jp>
2024-01-16 14:27:24 +0900

    直した2

--- src/web/prod_config.js
+++ src/web/prod_config.js
@@ -1,13 +1,5 @@
-const fs = require('fs');
-
-module.exports = {
-  production: true,
-  sshConfig: {
-    host: 'dev.marathon.rplearn.net',
-    port: 22, // 本番環境のSSHポート
-    user: 'risa_kikuchi',
-    privateKey: fs.readFileSync('/home/risa_kikuchi/.ssh/id_ed25519', 'utf8'),
-  },
-  deployScriptPath: '/app/deploy_prod.sh', // 本番環境のデプロイスクリプトのパス
-  
+const config = {
+  apiUrl: '/api_risa_kikuchi'
 };
+
+export default config;
